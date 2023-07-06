42f6855b1c0ac86f0ba64d4651669fcae39c277d
Ayomi <ayomi@swivelgroup.com.au>
2023-07-06 12:03:39 +0530

    seranity configuration

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -4,6 +4,22 @@ import "./index.css";
 import App from "./App";
 import { Provider } from "react-redux";
 import { store } from "./store/index";
+import * as Sentry from "@sentry/react";
+Sentry.init({
+  dsn: "https://b2472606cda943c9a762c5ac01a3bb4c@o4505480944549888.ingest.sentry.io/4505481027649536",
+  integrations: [
+    new Sentry.BrowserTracing({
+      // Set `tracePropagationTargets` to control for which URLs distributed tracing should be enabled
+      tracePropagationTargets: ["localhost", /^https:\/\/yourserver\.io\/api/],
+    }),
+    new Sentry.Replay(),
+  ],
+  // Performance Monitoring
+  tracesSampleRate: 1.0, // Capture 100% of the transactions, reduce in production!
+  // Session Replay
+  replaysSessionSampleRate: 0.1, // This sets the sample rate at 10%. You may want to change it to 100% while in development and then sample at a lower rate in production.
+  replaysOnErrorSampleRate: 1.0, // If you're not already sampling the entire session, change the sample rate to 100% when sampling sessions where errors occur.
+});
 
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
